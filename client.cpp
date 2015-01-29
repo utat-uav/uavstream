6ded4ad3d5abac0e67ccc535c409d941d0db3f12
Subtlemon <harryliang1@gmail.com>
2015-01-29 18:40:29 -0500

    client and server outline (sends one file with no error handling)

---
+++ client.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+
+using namespace std;
+
+#define BLOCK 1500
+
+void error(string msg){
+    cerr << msg << endl;
+    exit(0);
+}
+
+int main()
+{
+    //declarations
+    int sockfd, portno;
+    struct sockaddr_in serv_addr;
+    struct hostent* server;
+    char buffer[BLOCK];
+    //file IO declarations
+    int fSize;
+    char fName[256];
+
+    //get ip and port number
+    cout << "Server IP: ";
+    memset(fName, 0, 256);
+    cin >> fName;
+    server = gethostbyname("localhost");
+    cout << "Port number: ";
+    cin >> fName;
+    portno = atoi(fName);
+
+    //create socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    //test if socket and server exist
+    if(sockfd<0)
+        error("Error opening socket");
+    if(!server){
+        cout<< "No such host" << endl;
+        exit(0);
+    }
+
+    //ok I give up now
+    bzero((char*) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char*)server->h_addr, (char*)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if(connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))<0)
+        error("Error connecting");
+
+    //read file size and name
+    if(read(sockfd, buffer, BLOCK)<0)
+        error("Error reading size");
+    fSize = atoi(buffer);
+    cout << fSize << " bytes" << endl; //remove
+    if(write(sockfd, buffer, strlen(buffer))<0)
+        error("Error confirming size");
+    if(read(sockfd, fName, 256)<0)
+        error("Error reading name");
+    cout << fName << endl;
+    if(write(sockfd, fName, strlen(fName))<0)
+        error("Error confirming size");
+
+    //open output filestream
+    ofstream fileO(fName, ios::binary);
+    int c; //future error handling?
+    for(c=fSize/BLOCK;c>0;c--){
+        if(read(sockfd, buffer, BLOCK)<0)
+            error("error reading block");
+        fileO.write(buffer, BLOCK);
+        if(write(sockfd, buffer, BLOCK)<0)
+            error("error writing block");
+    }
+    if(read(sockfd, buffer, fSize%BLOCK)<0)
+        error("error reading last block");
+    fileO.write(buffer, BLOCK);
+    if(write(sockfd, buffer, fSize%BLOCK)<0)
+        error("error writing last block");
+
+    //close socket
+    close(sockfd);
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+
+using namespace std;
+
+#define BLOCK 1500
+
+void error(string msg){
+    cerr << msg << endl;
+    exit(0);
+}
+
+void getName(char* fpathstart){
+    int offset = 0;
+    char* ptr = fpathstart;
+    for(int c = 0; *ptr; c++, ptr++)
+        if(*ptr=='/')
+            offset = c+1;
+    while((*fpathstart=*(fpathstart+offset)))fpathstart++;
+}
+
+int main()
+{
+    //declarations
+    int sockfd, newsockfd, portno, clilen;
+    char buffer[BLOCK];
+    struct sockaddr_in serv_addr, cli_addr;
+    //file declarations
+    int fSize;
+    char fName[256];
+
+    //create input stream
+    cout << "Path to file: ";
+    cin >> fName;
+    ifstream fileI(fName, ios::ate|ios::binary);
+    if(!fileI.is_open())
+        error("file not found");
+
+    //create and test socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd<0)
+        error("Error opening socket");
+
+    //create port
+    cout << "port number (2000-65535): ";
+    cin >> portno;
+
+    //bind socket to specified port
+    bzero((char*)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if(bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))<0)
+        error("Error on binding");
+
+    //wait for client to connect
+    cout << "listening" << endl; //remove
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, (socklen_t*)&clilen);
+    if(newsockfd<0)
+        error("Error on accept");
+
+    //send file size and name
+    cout << "sending file size" << endl;
+    bzero(buffer, BLOCK);
+    sprintf(buffer, "%d", fSize=(int)fileI.tellg());
+    if(write(newsockfd, buffer, strlen(buffer))<0)
+        error("Error sending size");
+    read(newsockfd, buffer, 255);
+    cout << "sending file name" << endl;
+    getName(fName);
+    if(write(newsockfd, fName, strlen(fName))<0)
+        error("Error sending name");
+    read(newsockfd, buffer, 255);
+
+    //send file info to client
+    fileI.seekg(0, ios::beg);
+    int c; //future error handling?
+    for(c=fSize/BLOCK;c>0;c--){
+        fileI.read(buffer, BLOCK);
+        if(write(newsockfd, buffer, BLOCK)<0)
+            error("error writing block");
+        if(read(newsockfd, buffer, BLOCK)<0)
+            error("error reading block");
+    }
+    fileI.read(buffer, BLOCK);
+    if(write(newsockfd, buffer, fSize%BLOCK)<0)
+        error("error writing last block");
+    if(read(newsockfd, buffer, fSize%BLOCK)<0)
+        error("error reading last block");
+    fileI.close();
+
+    //finished tasks
+    close(sockfd);
+    close(newsockfd);
+    cout << "tasks completed successfully" << endl;
+    return 0;
+}
