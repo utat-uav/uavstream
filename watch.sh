b9cbcf508e7861a54ca2247c67ca1086ca36d933
Wesley <wesley@heungs.com>
2015-10-20 14:14:55 -0400

    Added file filtering via regex and integrated scp. Filenames to be ignored should be stored as a list of regexes in ignore_list. scp is used to copy the file to a remote address as specified.

--- watch.sh
+++ watch.sh
@@ -1,6 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+REMOTE="UTORID@remote.ecf.utoronto.ca:~/Desktop/"
+
 while true
 do
 	file=`inotifywait ./ -r -e CLOSE_WRITE 2> /dev/null | grep -oE '[^ ]+$'`
-	echo "$file"
+	echo "Detected $file changed."
+	if echo "$file" | grep -v -q -f "ignore_list"
+	then
+		echo "Copying $file to $REMOTE$file..."
+		scp $file $REMOTE$file
+	fi
 done
